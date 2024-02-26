e0a0c82a306b0d7788b3fa96a5651ec5a7d4c933
Lavanya <lavanyavangipurapu@gmail.com>
2024-02-26 22:30:14 +0530

    first commit

---
+++ mini-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Button from '@mui/material/Button';
+
+function App() {
+  
+
+  return (
+    <>
+      <Button variant="contained">Click Me</Button>
+    </>
+  )
+}
+
+export default App
